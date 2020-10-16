d3a622ad5c2b14eda8ad5770dbaba9ac6887b465
Beksultan <beksultankaiypov@gmail.com>
2020-10-16 23:04:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,26 +14,11 @@ end
 def get_english_meaning(file, emoticon)
   library_check = load_library(file)
   english_meaning = nil
-  sorry_message = "Sorry, that emoticon was not found"
-  library_check.each do |meaning, emoticon_language|
-    emoticon_language.each do |language, face|
-      if emoticon == face 
-        english_meaning = meaning
-      end
-    end
-end
+ 
 end 
 
 
 def get_japanese_emoticon(file, emoticon)
   check_emoticon = load_library(file)
-  japanese_emoticon = nil 
-  sorry_message = "Sorry, that emoticon was not found"
-  check_emoticon.each do |meaning, emoticon_language|
-    emoticon_language.each do |language, face|
-      if emoticon == face
-        japanese_emoticon = emoticon_language[:japanese]
-      end
-  end 
-end 
+
 end
